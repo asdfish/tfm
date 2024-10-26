3fd36bcd2216e60c541f9322c0c555949526f1c1
asdfish <lgray3420@gmail.com>
2024-10-26 08:30:46 -0400

    removed actions

--- src/main.c
+++ src/main.c
@@ -1,17 +1,8 @@
-#include <actions.h>
 /*#include <utils.h>*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/*extern int actions_add(enum ActionType type, const char** arguments, unsigned int argument_length);*/
 int main(void) {
-  if(actions_create_path("asdf/") != 0) {
-    printf("error\n");
-    actions_free();
-    return -1;
-  }
-
-  actions_free();
   return 0;
 }
