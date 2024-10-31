45d054c17a74b4a0d3c97fad6cee2a80f9abf986
asdfish <lgray3420@gmail.com>
2024-10-31 16:27:51 -0400

    made code more dry

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <macros.h>
 #include <menu.h>
 #include <dirent.h>
 #include <stdbool.h>
@@ -16,12 +17,16 @@ extern int create_path(const char* path);
 extern int dirents_to_menu_items(struct dirent** dirents, unsigned int dirents_length, struct MenuItem** output);
 extern int get_dirents(const char* path, struct dirent*** output, unsigned int* output_length);
 extern int get_dirent_names_recursive(const char* path, const char*** output, unsigned int* output_length);
-extern int get_directories(const char* path, const char*** output, unsigned int* output_length);
-extern int get_files(const char* path, const char*** output, unsigned int* output_length);
 extern int sentence_separate(const char* sentence, const char*** output, unsigned int* output_length);
 extern unsigned int string_count(const char* string, const char* query);
 extern bool path_exists(const char* path);
 extern int path_separate(const char* path, const char*** directories, unsigned int* directories_length, const char** file);
 extern enum PathType path_type(const char* path);
+
+extern GENERATE_GET_DIRENT_NAMES_FUNCTION_DEFINITION(get_directory_names);
+extern GENERATE_GET_DIRENT_NAMES_FUNCTION_DEFINITION(get_file_names);
+
+extern GENERATE_GET_DIRENTS_LENGTH_FUNCTION_DEFINITION(get_directories_length);
+extern GENERATE_GET_DIRENTS_LENGTH_FUNCTION_DEFINITION(get_files_length);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,65 +1,12 @@
 #define INCLUDE_CONFIG_FOREGROUNDS
 #include <config.h>
 #include <macros.h>
-#include <string_vector.h>
 #include <utils.h>
 #include <orchestra.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-
-#define GENERATE_DIRENT_FUNCTION(condition) {                                                   \
-  DIR* directory_pointer = opendir(path);                                                       \
-  if(directory_pointer == NULL)                                                                 \
-    goto exit_failure;                                                                          \
-                                                                                                \
-  struct dirent* dirent_pointer = NULL;                                                         \
-                                                                                                \
-  unsigned int dirent_names_length = 0;                                                         \
-  while((dirent_pointer = readdir(directory_pointer)) != NULL)                                  \
-    if(condition)                                                                               \
-      dirent_names_length ++;                                                                   \
-  closedir(directory_pointer);                                                                  \
-  directory_pointer = NULL;                                                                     \
-                                                                                                \
-  const char** dirent_names = (const char**) malloc(dirent_names_length * sizeof(const char*)); \
-  if(dirent_names == NULL)                                                                     \
-    goto exit_failure;                                                                          \
-                                                                                                \
-  directory_pointer = opendir(path);                                                            \
-  if(directory_pointer == NULL)                                                                 \
-    goto free_dirent_names;                                                                     \
-                                                                                                \
-  unsigned int i = 0;                                                                           \
-  while((dirent_pointer = readdir(directory_pointer)) != NULL)                                  \
-    if(condition) {                                                                             \
-      dirent_names[i] = strdup(dirent_pointer->d_name);                                         \
-      if(dirent_names[i] == NULL)                                                               \
-        goto free_dirent_names_contents;                                                        \
-      i ++;                                                                                     \
-    }                                                                                           \
-  closedir(directory_pointer);                                                                  \
-  directory_pointer = NULL;                                                                     \
-                                                                                                \
-  *output = dirent_names;                                                                       \
-  *output_length = dirent_names_length;                                                         \
-                                                                                                \
-  return 0;                                                                                     \
-                                                                                                \
-free_dirent_names_contents:                                                                     \
-  for(unsigned int j = 0; j < i; j ++) {                                                        \
-    free((char*) dirent_names[i]);                                                              \
-    dirent_names[i] = NULL;                                                                     \
-  }                                                                                             \
-  closedir(directory_pointer);                                                                  \
-  directory_pointer = NULL;                                                                     \
-free_dirent_names:                                                                              \
-  free(dirent_names);                                                                           \
-  dirent_names = NULL;                                                                          \
-exit_failure:                                                                                   \
-  return -1;                                                                                    \
-}
 
 int create_file(const char* path) {
   if(path_exists(path))
@@ -208,16 +155,22 @@ failure_exit:
 }
 
 int get_dirent_names_recursive(const char* path, const char*** output, unsigned int* output_length) {
-  return -1;
+
+
+  return 0;
 }
 
-int get_directories(const char* path, const char*** output, unsigned int* output_length) 
-  GENERATE_DIRENT_FUNCTION(dirent_pointer->d_type == DT_DIR &&
-      strcmp(dirent_pointer->d_name, ".") != 0 &&
-      strcmp(dirent_pointer->d_name, "..") != 0)
+GENERATE_GET_DIRENT_NAMES_FUNCTION_DEFINITION(get_directory_names)
+  GENERATE_GET_DIRENT_NAMES_FUNCTION_SOURCE(IS_DIRECTORY_CONDITION)
 
-int get_files(const char* path, const char*** output, unsigned int* output_length)
-  GENERATE_DIRENT_FUNCTION(dirent_pointer->d_type == DT_REG)
+GENERATE_GET_DIRENT_NAMES_FUNCTION_DEFINITION(get_file_names)
+  GENERATE_GET_DIRENT_NAMES_FUNCTION_SOURCE(IS_FILE_CONDITION)
+
+GENERATE_GET_DIRENTS_LENGTH_FUNCTION_DEFINITION(get_directories_length)
+  GENERATE_GET_DIRENTS_LENGTH_FUNCTION_SOURCE(IS_DIRECTORY_CONDITION)
+
+GENERATE_GET_DIRENTS_LENGTH_FUNCTION_DEFINITION(get_files_length)
+  GENERATE_GET_DIRENTS_LENGTH_FUNCTION_SOURCE(IS_FILE_CONDITION)
 
 int sentence_separate(const char* sentence, const char*** output, unsigned int* output_length) {
   if(strstr(sentence, " ") == NULL) {
